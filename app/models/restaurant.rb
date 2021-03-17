de809fe5c4361e7727c01298d7930eb3c1d6b06d
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-17 02:49:38 +0100

    Change restaurant column type and adding restaurant and review controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} invalid category" }
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than: 6 }
+  validates :content, presence: true
+  validates_associated :restaurant
+  
 end
